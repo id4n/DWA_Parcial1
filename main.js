f30571d057b525ce587f748c5b17d4a32da6c940
id4n <60779600+id4n@users.noreply.github.com>
2020-09-10 19:34:20 -0500

    Parcial1_v3

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const main = document.getElementById('main')
 const tittle = document.createElement('h1')
 const divImg = document.createElement('div')
 const divBtn = document.createElement('div')
+var img = document.createElement('img')
+divImg.appendChild(img)
+
 window.onload = function() {
     tittle.innerText = 'sign lenguaje'
     this.main.appendChild(tittle)
     this.main.appendChild(divImg)
     this.main.appendChild(divBtn)
 }
+
 const url = 'https://dwaapi.juvasquez88.vercel.app/letters'
 const http = new XMLHttpRequest()
 
@@ -18,18 +22,18 @@ http.onload = function() {
     const letters = http.response
     showBtn(letters)
   }
+
 function showBtn(jsonObj){
     const letras = jsonObj['letters']
 
     for (var i = 0; i < letras.length; i++) {
         const btn = document.createElement('button')
-        btn.setAttribute('onclick','postImg('+letras[i].image+')');
+        let image = letras[i].image
+        btn.onclick= function() { postImg(image)}
         btn.textContent = letras[i].letter
         divBtn.appendChild(btn)
     }
 }
-function postImg(url) {
-    const img = document.createElement('img')
-    img.setAttribute('src',url)
-    divImg.appendChild(img)
+function postImg(urlImg) {
+    img.setAttribute('src', urlImg)
 }
